c3bd8513c0cb52cd7fcd24fcf9cc76b822dc5b93
Brandon Miller <millbj92@gmail.com>
2019-03-10 21:39:50 -0400

    changes

---
+++ src/components/Contact.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
+
+  render() {
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name}{" "}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+            onClick={this.onDeleteClick}
+          />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {phone}</li>
+            <li className="list-group-item">Phone: {email}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
+};
+
+export default Contact;
